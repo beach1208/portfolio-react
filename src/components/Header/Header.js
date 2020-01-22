6ffbc94aede136287f78029f5c8f285583e4a598
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-22 15:55:11 +0900

    mixin function added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ function Header() {
     return (
         <div className="header">
             <div className="wrapper">
-                <h1 className="header__logo"><Link to="/">NK</Link></h1>
+                <h1 className="header__logo"><Link className="header__logo-link" to="/">NK</Link></h1>
                 <nav className="header__nav">
-                    <Link to="/" className="header_nav">Work</Link>
-                    <Link to="/about" className="header_nav">About</Link>
-                    <Link to="/contact" className="header_nav">Contact</Link>
+                    <Link to="/" className="header__nav-link">Work</Link>
+                    <Link to="/about" className="header__nav-link">About</Link>
+                    <Link to="/contact" className="header__nav-link">Contact</Link>
                 </nav>
             </div>
         </div>
