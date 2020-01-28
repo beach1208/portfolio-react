43e3fc655d11d2d8e1218b89d3e3d7e81615868b
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-28 10:57:11 +0900

    Replaced images

--- src/theme/assets.js
+++ src/theme/assets.js
@@ -2,14 +2,11 @@ export const Assets = {
   p1preview: require('../components/img/p1preview.jpg'),
   p1detail: require('../components/img/p1-detail.png'),
   p2preview: require('../components/img/p2preview.png'),
-  p2detail: require('../components/img/p2detail.png'),
   p3preview: require('../components/img/p3preview.png'),
   p3detail: require('../components/img/p3detail.png'),
-  p4preview: require('../components/img/p4preview.png'),
+  p4preview: require('../components/img/p4preview.jpg'),
   p4detail: require('../components/img/p4detail.jpg'),
-  p5preview: require('../components/img/p5preview.png'),
-  p5detail: require('../components/img/p5detail.png'),
+  p5preview: require('../components/img/p5preview.jpg'),
   p6preview: require('../components/img/p6preview.png'),
-  p6detail: require('../components/img/p6detail.png'),
   profileImg: require('../components/img/profile.jpg')
 }
