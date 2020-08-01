3ad9df942b7c769c7f8c9c072ddc7653be8ead09
beach1208 <nk.bg.1208@gmail.com>
2020-08-01 12:24:56 +0900

    Fixed header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,33 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../sass/app.scss";
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { language: false }
-    }
-    handleClick = () => {
-        this.setState(state => ({
-          language: !state.language
-        }));
-    }
-    
-    render() {
-        console.log(this.state.language)
-        return (
-            <div className="header">
-                <div className="container">
-                    <div className="header__logo"><Link className="header__logo-link" to="/">Nagisa.K</Link></div>
-                    <nav className="header__nav">
-                        <Link to="/" className="header__nav-link">Work</Link>
-                        <Link to="/about" className="header__nav-link">About</Link>
-                        <Link to="/contact" className="header__nav-link">Contact</Link>
-                        <button value={this.state.language} onClick={this.handleClick}>JPN</button>
-                    </nav>
-                </div>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="header__logo"><Link className="header__logo-link" to="/">Nagisa.K</Link></div>
+                <nav className="header__nav">
+                    <Link to="/" className="header__nav-link">Work</Link>
+                    <Link to="/about" className="header__nav-link">About</Link>
+                    <Link to="/contact" className="header__nav-link">Contact</Link>
+                </nav>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Header;
