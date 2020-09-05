0fc50441a0443acd2ac975272e22aead337564da
beach1208 <nk.bg.1208@gmail.com>
2020-09-05 16:02:35 +0900

    Added a new project

--- src/theme/assets.js
+++ src/theme/assets.js
@@ -1,12 +1,13 @@
 export const Assets = {
   p1preview: require('../components/img/p1preview.jpg'),
-  p1detail: require('../components/img/p1-detail.png'),
-  p2preview: require('../components/img/p2preview.png'),
+  p2preview: require('../components/img/p2preview.jpg'),
+  p2detail: require('../components/img/p2detail.jpg'),
   p3preview: require('../components/img/p3preview.png'),
   p3detail: require('../components/img/p3detail.png'),
-  p4preview: require('../components/img/p4preview.jpg'),
-  p4detail: require('../components/img/p4detail.jpg'),
+  p4preview: require('../components/img/p4preview.png'),
   p5preview: require('../components/img/p5preview.jpg'),
-  p6preview: require('../components/img/p6preview.png'),
+  p5detail: require('../components/img/p5detail.png'),
+  p6preview: require('../components/img/p6preview.jpg'),
+  p6detail: require('../components/img/p6detail.jpg'),
   profileImg: require('../components/img/profile.jpg')
 }
